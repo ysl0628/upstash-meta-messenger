9ec2e29944ba420d8ded401107c69465f148004b
ysl0628 <yihsinlan@gmail.com>
2023-01-04 19:21:53 +0800

    feat: add images domains setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['links.papareact.com'], // 需要設定允許的網域
+  },
+  experimental: {
+    appDir: true,
+  },
 }
