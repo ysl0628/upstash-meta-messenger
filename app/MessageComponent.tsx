81ace3c07898fabc9939e7de24e9e9620aeb4ff6
ysl0628 <yihsinlan@gmail.com>
2023-01-08 11:44:36 +0800

    feat: traverse messages from api on MessageList

---
+++ app/MessageComponent.tsx
@@ -0,0 +1,53 @@
+import { Message } from '../typings'
+import Image from 'next/image'
+
+type Props = {
+  message: Message
+}
+const MessageComponent = ({ message }: Props) => {
+  const isUser = true
+
+  return (
+    <div className={`flex w-fit ${isUser && 'ml-auto'}`}>
+      <div className={`flex-shrink-0 ${isUser && 'order-2'}`}>
+        <Image
+          className="mx-2 rounded-full"
+          height={10}
+          width={50}
+          src={message.profilePic}
+          alt="Profile Picture"
+          priority
+        />
+      </div>
+
+      <div>
+        <p
+          className={`text-[0.65rem] px-[2px] pb-[2px] ${
+            isUser ? 'text-blue-400 text-right' : 'text-red-400 text-left'
+          }`}
+        >
+          {message.username}
+        </p>
+
+        <div className="flex items-end">
+          <div
+            className={`px-3 py-2 rounded-lg w-fit text-white ${
+              isUser ? 'bg-blue-400 ml-auto order-2' : 'bg-red-400'
+            } `}
+          >
+            <p>{message.message}</p>
+          </div>
+          <p
+            className={`text-[0.65rem] italic px-2 text-gray-300 ${
+              isUser && 'text-left'
+            }`}
+          >
+            {new Date(message.created_at).toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MessageComponent
--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -1,7 +1,24 @@
+'use client'
 import React from 'react'
+import useSWR from 'swr'
+import { Message } from '../typings'
+import fetcher from '../utils/fetchMessage'
+import MessageComponent from './MessageComponent'
 
 const MessageList = () => {
-  return <div>MessageList</div>
+  const {
+    data: messages,
+    error,
+    mutate,
+  } = useSWR<Message[]>('api/getMessages', fetcher)
+
+  return (
+    <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
+      {messages?.map((message) => (
+        <MessageComponent key={message.id} message={message} />
+      ))}
+    </div>
+  )
 }
 
 export default MessageList
