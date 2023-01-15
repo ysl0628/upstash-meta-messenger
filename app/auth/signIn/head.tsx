16f81f4ae7af4433419253339c33d1a4850a5cc6
ysl0628 <yihsinlan@gmail.com>
2023-01-15 16:27:55 +0800

    feat: update head tags

--- app/auth/signIn/head.tsx
+++ app/auth/signIn/head.tsx
@@ -1,3 +1,16 @@
 export default function Head() {
-  return <title>Sign in to Messenger</title>
+  return (
+    <>
+      <title>Sign in to Messenger</title>
+      <meta property="fb:app_id" content="1234567890" />
+      <meta property="og:type" content="article" />
+      <meta
+        property="og:url"
+        content="	https://upstash-meta-messenger-neon.vercel.app/auth/signIn?callbackUrl=https%3A%2F%2Fupstash-meta-messenger-neon.vercel.app%2F"
+      />
+      <meta property="og:title" content="upstash-meta-messenger" />
+      <meta property="og:image" content="https://links.papareact.com/161" />
+      <meta property="og:description" content="upstash-meta-messenger" />
+    </>
+  )
 }
