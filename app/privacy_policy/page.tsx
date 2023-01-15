a74b86e50a351e08cb58c79779fc39ce95ac3bf9
ysl0628 <yihsinlan@gmail.com>
2023-01-15 15:52:42 +0800

    feat: build privacy_policy page

---
+++ app/privacy_policy/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const PrivacyPolicyPage = () => {
+  return (
+    <>
+      <h4>
+        本網站登入有使用 Facebook 登入應用。根據 Facebook 的政策，我們必須提供
+        "Facebook資料刪除指示的網址" 這個說明網頁。
+      </h4>
+      <h4>
+        如果您想刪除與 upstash-messager 有關的 Facebook
+        登入應用數據資料，您可以按照以下步驟進行：
+      </h4>
+      <ul>
+        <li>進入您的 Facebook 帳號的 "設定和隱私" 選單。</li>
+        <li>點擊 "設定"。</li>
+        <li>向下滾動並點擊 "應用程式和網站"。</li>
+        <li>找到並點擊 "upstash-messager"。</li>
+        <li>點擊 "移除" 按鈕。</li>
+        <li>即可成功地刪除了您的 upstash-messager 應用程式活動。</li>
+      </ul>
+    </>
+  )
+}
+
+export default PrivacyPolicyPage
